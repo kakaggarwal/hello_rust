5b682ed230a45e06605ef18e774a9456f9061800
Krishan Aggarwal <kakaggarwal@gmail.com>
2026-08-06 07:36:32 +0530

    Add personal introduction details to main function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
     // because the println ends with ! which means its a marco
     println!("Hello, world!");
+
+    println!("=============================================");
+    println!("Name:     {}", "Krishan Aggarwal");
+    println!("Goal:     {}", "Become a Rust Systems Engineer");
+    println!("Country:  {}", "Bharat");
+    println!("=============================================");
 }
