21e5a13d8285ed99d24df4c57a3e42ed899d7aea
Krishan Aggarwal <kakaggarwal@gmail.com>
2026-08-22 16:40:47 +0530

    feat: add value moving example and enhance variable usage demonstration

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     println!("=============================================");
 
     variables();
+    value_moving();
 }
 
 fn variables() {
@@ -30,4 +31,31 @@ fn variables() {
     println!("Mail Count: {}", mail_count);
     // This is an example of shadowing. We have declared a new variable with the same name as the previous one. 
     // The previous variable is shadowed by the new one.
+
+    let name: String = String::from("Alice");
+    let age: i32 = 30;
+    let salary: f64 = 50000.0;
+    let is_active: bool = true;
+    let grade: char = 'A';
+
+    let employee = (name, age, salary, is_active, grade);
+    let marks = [90, 85, 95];
+
+    // println!("Name: {}", name);
+    // Above code will not compile as name variable has been moved to employee tuple. We can access the name variable from the employee tuple.
+    println!("Name: {}", employee.0);
+    println!("Age: {}", employee.1);
+    println!("Marks: {}", marks[1]);
+
+}
+
+fn value_moving() {
+    let x = 5;
+    let y = x; // Here the value of x is copied to y. This is because i32 implements the Copy trait.
+    println!("x: {}, y: {}", x, y);
+
+    let s1 = String::from("Hello");
+    let s2 = s1; // Here the value of s1 is moved to s2. This is because String does not implement the Copy trait.
+    // println!("s1: {}, s2: {}", s1, s2); // This line will result in error as s1 is no longer valid.
+    println!("s2: {}", s2);
 }
